94efece9936fd5b61763e636f6aa9fafe17d6bbe
Steve Minor <stv.mnr@gmail.com>
2014-10-01 14:10:13 -0700

    using jquery for weird click event issue with safari. odd rendering issues with firefox related to inline-block elements

--- js/hd.js
+++ js/hd.js
@@ -1,25 +1,43 @@
-var cube = document.querySelector('#cube'),
-	classes = [
-		'show-top',
-		'show-left',
-		'show-front',
-		'show-right',
-		'show-bottom',
-		'show-back'
-	],
-	index = 0;
+var cubeElements = document.querySelectorAll('.cube'),
+	rows = document.querySelectorAll('.row'),
+	cubes = [];
 
-function setClass(className){
-	cube.className = className;
+function HampsterCube(element, startIndex){
+	this.index = startIndex;
+	this.element = element;
+	this.$element = $(element);
+	this.boundAdvance = this.advance.bind(this);
+	this.$element.on('click', this.boundAdvance);
+	this.$element.on('rotate', this.boundAdvance);
 }
 
-var spinInterval = setInterval(function(){
-	setClass(classes[index]);
-	index = (index + 1) % classes.length;
-}, 2000);
+HampsterCube.prototype.classes = [
+	'show-top',
+	'show-left',
+	'show-front',
+	'show-right',
+	'show-bottom',
+	'show-back'
+];
 
-/* hampsterCube object
-	
-	currentIndex;
+HampsterCube.prototype.advance = function(event){
+	event.stopPropagation();
+	this.index = (this.index + 1) % this.classes.length;
+	this.element.className = "cube " + this.classes[this.index];
+}
 
-*/
+// var spinInterval = setInterval(function(){
+// 	cubes.forEach(function(hampsterCube){
+// 		hampsterCube.advance();
+// 	});
+// }, 2000);
+
+Array.prototype.forEach.call(cubeElements, function(cubeElement){
+	cubes.push(new HampsterCube(cubeElement, 0));
+});
+
+Array.prototype.forEach.call(rows, function(row){
+		// var spinner = setInterval(function(){
+		// 	var 
+		// }, 10000);
+})
